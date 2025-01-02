8a15ede926f9eb8edf359b0c32157c6bafac25bd
kabirasunny <sunnykabira5507@gmail.com>
2025-01-02 17:18:57 +0530

    I added conponent of footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/navbar'
+import Footer from './components/Footer'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
   return (
     <>
       <RouterProvider router={router} />
+      <Footer/>
     </>
   )
 }
